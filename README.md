38356f23b2cba29d032e04d2ae7408c525527fd7
windowfinn <stefanpacevitch@gmail.com>
2016-03-29 21:42:32 +0100

    First commit of code

---
+++ README.md
@@ -0,0 +1,41 @@
+# esp8266-wifi-plug
+
+This Arduino language code is intended for an ESP8266 chip that controls a relay on GPIO2 and an LED(s) on GPIO0. The relay controlling if mains voltage reaches a device or not.
+
+Once the Wifi settings are configured via the captive portal the user can connect to the 'plug' and is presented with a webpage that allows them to toggle the relay, and whatever it controls, on and off.
+
+## Installation
+The data folder contains some fonts, and is thus quite large. To use the project as is you will need an ESP8266 chip with at least 1MB flash memory. The author specified a 512k SPIFFS in the settings in the Arduino IDE.
+
+A tool is required to upload the html and associated files to the ESP8266 from the Arduino IDE - see http://esp8266.github.io/Arduino/versions/2.0.0/doc/filesystem.html
+
+1. Flash the code to the chip
+2. Upload the data folder
+
+## Usage
+
+Assuming that 2 LEDS have been wired to GPIO0 (such that one is off while the other is on when the pin is set to HIGH or LOW)...
+
+1. The two leds will flash alternately indicating that the ESP8266 is ready to be configured.
+2. From a wifi enabled device, look for the Access Point created by the chip, and connect to it.
+3. Try to visit any page, and you will be presented with the WifiManager configuration page.
+4. After the appropriate credentials have been supplied, the ESP8266 will reboot.
+5. Reconnect to your Wifi, and in a browser enter, 'http://esp8266.local' - this should present you with a CSS button that allows the user to toggle the state of GPIO2, which is connected to a relay.
+
+## History
+
+* Started as a fork of esp-arduino-boot
+* Further inspired to fork ESP8266_WebSockets_NeoPixels
+* Learned of WifiManager
+* Full rewrite based on WifiManager with pages served from the FS
+* Cool Button added from tutorial - Create A Sleek On/Off Button
+
+## Credits
+
+* esp-arduino-apboot - https://github.com/chriscook8/esp-arduino-apboot
+* ESP8266_WebSockets_NeoPixels - https://github.com/AdySan/ESP8266_WebSockets_NeoPixels/
+* WifiManager - https://github.com/tzapu/WiFiManager
+* 2 LED's with One Output - http://forum.arduino.cc/index.php?topic=356038.0
+* example README.md - https://gist.github.com/zenorocha/4526327
+* Font Awesome - http://fortawesome.github.io/Font-Awesome/
+* CSS3 Tutorial: Create A Sleek On/Off Button - http://www.hongkiat.com/blog/css3-on-off-button/
---
+++ esp8266-wifi-plug.ino
@@ -0,0 +1,218 @@
+#include <ESP8266WiFi.h>
+
+//needed for WifiManager
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+//for WifiManager LED status
+#include <Ticker.h>
+
+#include <ESP8266mDNS.h>
+#include <WiFiClient.h>
+#include <FS.h>
+
+//PINs on the Arduino
+#define LED_0 0 // LED PIN
+#define RELAY 2 // Relay PIN
+
+#define USE_SERIAL Serial
+
+Ticker ticker;
+
+// multicast DNS responder
+MDNSResponder mdns;
+
+// TCP server at port 80 will respond to HTTP requests
+ESP8266WebServer server(80);
+
+//WiFiManager
+//Local intialization. Once its business is done, there is no need to keep it around
+WiFiManager wifiManager;
+
+const String a404Html = "HTTP/1.1 404 Not Found\r\n\r\n";
+const String openingHtml = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>";
+const String closingHtml = "</html>\r\n\r\n";
+
+void tick()
+{
+  //toggle state
+  int state = digitalRead(LED_0);  // get the current state of GPIO1 pin
+  digitalWrite(LED_0, !state);     // set pin to the opposite state
+}
+
+//gets called when WiFiManager enters configuration mode
+void configModeCallback (WiFiManager *myWiFiManager) {
+  USE_SERIAL.println("Entered config mode");
+  USE_SERIAL.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  USE_SERIAL.println(myWiFiManager->getConfigPortalSSID());
+  //entered config mode, make led toggle faster
+  ticker.attach(0.2, tick);
+}
+
+//format bytes
+String formatBytes(size_t bytes){
+  if (bytes < 1024){
+    return String(bytes)+"B";
+  } else if(bytes < (1024 * 1024)){
+    return String(bytes/1024.0)+"KB";
+  } else if(bytes < (1024 * 1024 * 1024)){
+    return String(bytes/1024.0/1024.0)+"MB";
+  } else {
+    return String(bytes/1024.0/1024.0/1024.0)+"GB";
+  }
+}
+
+String getContentType(String filename){
+  if(server.hasArg("download")) return "application/octet-stream";
+  else if(filename.endsWith(".htm")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".png")) return "image/png";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";
+  else if(filename.endsWith(".zip")) return "application/x-zip";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  else if(filename.endsWith(".eot")) return "application/vnd.ms-fontobject";
+  else if(filename.endsWith(".svg")) return "image/svg+xml";
+  else if(filename.endsWith(".ttf")) return "application/octet-stream";
+  else if(filename.endsWith(".woff")) return "application/x-font-woff";
+  return "text/plain";
+}
+
+bool handleFileRead(String path){
+  USE_SERIAL.println("handleFileRead: " + path);
+  if(path.endsWith("/")) path += "index.htm";
+  USE_SERIAL.println("handleFileRead: " + path);
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){
+    USE_SERIAL.println("File exists: " + path);
+    if(SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
+
+void handleOn() {
+  digitalWrite(RELAY, LOW);
+  digitalWrite(LED_0, HIGH);
+  USE_SERIAL.println("Sending 200");
+  handleFileRead("/");
+}
+
+void handleOff() {
+  digitalWrite(RELAY, HIGH);
+  digitalWrite(LED_0, LOW);
+  USE_SERIAL.println("Sending 200");
+  handleFileRead("/");
+}
+
+void handleClear() {
+  String s = openingHtml;
+  s += "Hello from ESP8266";
+  s += "<p>Clearing the settings.<p>";
+  s += closingHtml;
+  USE_SERIAL.println("Sending 200");
+  server.send(200, "text/plain", s);
+
+  wifiManager.resetSettings();
+
+  WiFi.disconnect();      //after EEPROM is cleared, disconnect from the current wifi access
+  ESP.reset();
+}
+
+void setup() {
+  USE_SERIAL.begin(115200);
+
+  //Setup LED Diagnostic
+  pinMode(LED_0, OUTPUT);
+
+  //Relay pin
+  pinMode(RELAY, OUTPUT);
+
+  // start ticker with 0.5 because we start in AP mode and try to connect
+  ticker.attach(0.6, tick);
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  //WiFiManager wifiManager;
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+
+  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  wifiManager.setAPCallback(configModeCallback);
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect()) {
+    USE_SERIAL.println("failed to connect and hit timeout");
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(1000);
+  }
+
+  //if you get here you have connected to the WiFi
+  USE_SERIAL.println("Connected...");
+  ticker.detach();
+
+  //keep LED on
+  digitalWrite(LED_0, LOW);
+
+  SPIFFS.begin();
+  {
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next()) {    
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      USE_SERIAL.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
+    }
+    USE_SERIAL.printf("\n");
+  }
+
+  // Set up mDNS responder:
+  // - first argument is the domain name, in this example
+  //   the fully-qualified domain name is "esp8266.local"
+  // - second argument is the IP address to advertise
+  //   we send our IP address on the WiFi network
+  if (!mdns.begin("esp8266", WiFi.localIP())) {
+    USE_SERIAL.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  USE_SERIAL.println("mDNS responder started");
+
+  //Server Init
+  server.on("/on", HTTP_GET, handleOn);
+  server.on("/off", HTTP_GET, handleOff);
+  server.on("/clear",HTTP_GET, handleClear);
+
+  server.onNotFound([](){
+    if(!handleFileRead(server.uri()))
+      server.send(404, "text/plain", a404Html);
+  });
+
+  // Start TCP (HTTP) server
+  server.begin();
+  USE_SERIAL.println("TCP server started");
+}
+
+void loop() {
+
+  // Check for any mDNS queries and send responses
+  mdns.update();
+  server.handleClient();
+
+}
